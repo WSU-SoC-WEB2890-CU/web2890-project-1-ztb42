63b6ff35455f591539fd42d612d8dc0e930a1719
ztb42 <ztb42@hotmail.com>
2024-02-04 01:12:24 -0700

    final submission

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,24 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+
+document.addEventListener("DOMContentLoaded", function () {
+    var listItems = document.querySelectorAll("li")
+
+    function checkVisibility() {
+        listItems.forEach(function (item) {
+            var itemTop = item.getBoundingClientRect().top
+            var windowHeight = window.innerHeight
+
+            if (itemTop < windowHeight - 20) {
+                item.classList.add("visible")
+            } else {
+                item.classList.remove("visible")
+            }
+        })
+    }
+
+    window.addEventListener("scroll", checkVisibility)
+
+    // Initial check on page load
+    checkVisibility()
+})
